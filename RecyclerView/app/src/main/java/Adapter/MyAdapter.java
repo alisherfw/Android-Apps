11907ea0f799bde3b3465620ece6394eae8e4486
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-06-13 10:48:42 +0500

    Adapter and Model packages added

---
+++ RecyclerView/app/src/main/java/Adapter/MyAdapter.java
@@ -0,0 +1,45 @@
+package Adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+import Model.ListItem;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private Context context;
+    private List<ListItem> listItems;
+
+
+    public MyAdapter(Context context, List listitem) {
+    }
+
+    @NonNull
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+         public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
