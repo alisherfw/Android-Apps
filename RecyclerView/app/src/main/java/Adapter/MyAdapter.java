26f2bda98d2198f9aa004b3d2f15880ecc15467d
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-06-18 16:53:43 +0500

    MyAdapter updated

--- RecyclerView/app/src/main/java/Adapter/MyAdapter.java
+++ RecyclerView/app/src/main/java/Adapter/MyAdapter.java
@@ -1,12 +1,14 @@
 package Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.recyclerview.Details;
 import com.example.recyclerview.R;
 
 import org.w3c.dom.Text;
@@ -74,6 +76,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
             int position = getAdapterPosition();
 
             ListItem item = listItems.get(position);
+            Intent intent = new Intent(context, Details.class);
+            intent.putExtra("name", item.getName());
+            intent.putExtra("description", item.getDescription());
+            intent.putExtra("rating", item.getRating());
+
+            context.startActivity(intent);
 
             Toast.makeText(context, item.getName(), Toast.LENGTH_LONG).show();
         }
