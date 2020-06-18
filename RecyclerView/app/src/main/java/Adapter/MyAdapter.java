10f169ea0199bb085e3aa1ec073d2055d26731a6
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-06-18 16:21:01 +0500

    Added onClick() Listener

--- RecyclerView/app/src/main/java/Adapter/MyAdapter.java
+++ RecyclerView/app/src/main/java/Adapter/MyAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.recyclerview.R;
 
@@ -51,7 +52,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         return listItems.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public TextView name;
         public TextView description;
         public TextView rating;
@@ -59,11 +60,22 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            itemView.setOnClickListener(this);
+
             name = (TextView) itemView.findViewById(R.id.title);
             description = (TextView) itemView.findViewById(R.id.description);
             rating = (TextView) itemView.findViewById(R.id.rating);
 
+        }
 
+        @Override
+        public void onClick(View v) {
+//            Get postion of the row clicked or tapped.
+            int position = getAdapterPosition();
+
+            ListItem item = listItems.get(position);
+
+            Toast.makeText(context, item.getName(), Toast.LENGTH_LONG).show();
         }
     }
 }
