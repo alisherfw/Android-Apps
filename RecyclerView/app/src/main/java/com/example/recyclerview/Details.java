cba58a0d1d6c9f93506ed5a7ab1eeb90012050ea
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-06-18 16:59:25 +0500

    Added extras

--- RecyclerView/app/src/main/java/com/example/recyclerview/Details.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/Details.java
@@ -3,12 +3,32 @@ package com.example.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class Details extends AppCompatActivity {
+
+    private TextView name, description, rating;
+    private Bundle extras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        extras = getIntent().getExtras();
+
+        name = (TextView) findViewById(R.id.dNameId);
+        description = (TextView) findViewById(R.id.dDescriptionId);
+        rating = (TextView) findViewById(R.id.dRatingId);
+
+        if(extras != null) {
+            name.setText(extras.getString("name"));
+            description.setText(extras.getString("description"));
+            rating.setText(extras.getString("rating"));
+        }
+
+
+
     }
 }
