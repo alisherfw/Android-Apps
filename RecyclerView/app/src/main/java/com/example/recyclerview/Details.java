69ec7dfded6998d3869bea8ce908877a2900ee0e
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-06-20 10:59:48 +0500

    Added random colors

--- RecyclerView/app/src/main/java/com/example/recyclerview/Details.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/Details.java
@@ -2,14 +2,21 @@ package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class Details extends AppCompatActivity {
 
     private TextView name, description, rating;
     private Bundle extras;
+    private View windowView;
+    private int[] colors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +28,21 @@ public class Details extends AppCompatActivity {
         name = (TextView) findViewById(R.id.dNameId);
         description = (TextView) findViewById(R.id.dDescriptionId);
         rating = (TextView) findViewById(R.id.dRatingId);
+        windowView = (View) findViewById(R.id.windowViewID);
+
+        colors = new int[] {
+                Color.RED, Color.GRAY, Color.YELLOW, Color.GREEN, Color.BLACK, Color.BLUE,
+                Color.DKGRAY, Color.WHITE, Color.MAGENTA, Color.LTGRAY, Color.CYAN
+        };
+
 
         if(extras != null) {
             name.setText(extras.getString("name"));
             description.setText(extras.getString("description"));
             rating.setText(extras.getString("rating"));
+            Random random = new Random();
+            int randomNum = random.nextInt(colors.length);
+            windowView.setBackgroundColor(colors[randomNum]);
         }
-
     }
 }
