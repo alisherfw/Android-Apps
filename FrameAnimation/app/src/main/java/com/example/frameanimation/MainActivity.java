384d4176fb5f543f8e19404329b84df5318852a0
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-06-29 02:37:49 +0500

    startAnim changed

--- FrameAnimation/app/src/main/java/com/example/frameanimation/MainActivity.java
+++ FrameAnimation/app/src/main/java/com/example/frameanimation/MainActivity.java
@@ -6,6 +6,8 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,24 +19,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         batImage = (ImageView) findViewById(R.id.batId);
-        batImage.setBackgroundResource(R.drawable.bat_anim);
-        batAnimation = (AnimationDrawable) batImage.getBackground();
+//        batImage.setBackgroundResource(R.drawable.bat_anim);
+//        batAnimation = (AnimationDrawable) batImage.getBackground();
 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        batAnimation.start();
+//        batAnimation.start();
 
         Handler mHandler = new Handler();
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 //stop animation
-                batAnimation.stop();
+//                batAnimation.stop();
+                Animation startanimation = AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.fadein_animation);
+                batImage.startAnimation(startanimation);
+
             }
-        }, 5000);
+        }, 50);
 
         return super.onTouchEvent(event);
 
