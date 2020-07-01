532f233869d0cc2901f141a3715ccac7a3ea1b6c
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-07-01 20:57:40 +0500

    playMusic(), pauseMusic() created

--- MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -2,15 +2,43 @@ package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.theme_music);
 
-
+        playButton = (Button) findViewById(R.id.playButtonId);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()) {
+                    pauseMusic();
+                } else {
+                    startMusic();
+                }
+            }
+        });
+    }
+    public void pauseMusic() {
+        if(mediaPlayer != null) {
+            mediaPlayer.pause();
+            playButton.setText("Play");
+        }
+    }
+    public void startMusic() {
+        mediaPlayer.start();
+        playButton.setText("Pause");
     }
 }
