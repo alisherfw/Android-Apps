ce8f91f8b7db235f56cffc371d6ab5c3e22efa7d
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-07-01 21:16:43 +0500

    created mediaplayer.onCompletion() and onDestroy()

--- MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -6,6 +6,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mediaPlayer = new MediaPlayer();
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.theme_music);
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                int duration = mp.getDuration();
+                String mDuration = String.valueOf(duration / 1000);
+                Toast.makeText(MainActivity.this, "duration: "
+                        + mDuration, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         playButton = (Button) findViewById(R.id.playButtonId);
         playButton.setOnClickListener(new View.OnClickListener() {
@@ -42,5 +53,15 @@ public class MainActivity extends AppCompatActivity {
     public void startMusic() {
         mediaPlayer.start();
         playButton.setText("Pause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        super.onDestroy();
     }
 }
