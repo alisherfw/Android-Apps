afba20c9389ca5d0b69ed841ed35fcc7a150b6a7
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-07-05 23:34:45 +0500

    Buttons created

--- MusicBox/app/src/main/java/com/example/musicbox/MainActivity.java
+++ MusicBox/app/src/main/java/com/example/musicbox/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.musicbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button playButton, previousButton, nextButton;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playButton = (Button) findViewById(R.id.playButtonId);
+        previousButton = (Button) findViewById(R.id.previousButtonId);
+        nextButton = (Button) findViewById(R.id.nextButtonId);
+
 
     }
 }
