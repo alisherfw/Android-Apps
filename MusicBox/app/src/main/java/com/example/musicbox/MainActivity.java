56b97ed6a3a3256c37ec8e3f0fe7106c87563c5e
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-07-07 06:15:25 +0500

    created setupUI()

--- MusicBox/app/src/main/java/com/example/musicbox/MainActivity.java
+++ MusicBox/app/src/main/java/com/example/musicbox/MainActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.Scanner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button playButton, previousButton, nextButton;
     private MediaPlayer mediaPlayer;
@@ -24,21 +27,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playButton = (Button) findViewById(R.id.playButtonId);
-        previousButton = (Button) findViewById(R.id.previousButtonId);
-        nextButton = (Button) findViewById(R.id.nextButtonId);
+        setupUI();
+
+
+    }
+    public void setupUI() {
         mediaPlayer = new MediaPlayer();
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.music);
 
+        artistImage = (ImageView) findViewById(R.id.ovalimageId);
+        leftTime = (TextView) findViewById(R.id.leftTimeId);
+        rightTime = (TextView) findViewById(R.id.rightTimeId);
+        seekBar = (SeekBar) findViewById(R.id.seekBarId);
+        previousButton = (Button) findViewById(R.id.previousButtonId);
+        nextButton = (Button) findViewById(R.id.nextButtonId);
+        playButton = (Button) findViewById(R.id.playButtonId);
 
+        previousButton.setOnClickListener(this);
+        playButton.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.previousButtonId:
+
+                break;
+            case R.id.nextButtonId:
+
+                break;
+            case R.id.playButtonId:
+
+                break;
+        }
+    }
+    public void pauseMusic() {
 
     }
     public void playMusic() {
 
     }
-    public void pauseMusic() {
-
-    }
-
 }
