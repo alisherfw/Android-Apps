d5e78d32358415d68cbd9c9d664af2a7457e291f
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-07-08 06:06:17 +0500

    setupUI() created

--- internetIsConnected/app/src/main/java/com/example/internetisconnected/MainActivity.java
+++ internetIsConnected/app/src/main/java/com/example/internetisconnected/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.internetisconnected;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button checkButton;
+    private TextView statusText;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupUI();
+
+
     }
+    public void setupUI() {
+        checkButton = (Button) findViewById(R.id.checkButtonId);
+        statusText = (TextView) findViewById(R.id.statusTextId);
+        imageView = (ImageView) findViewById(R.id.imageViewId);
+    }
+
+    public boolean isInternetConnected() {
+        return false;
+    }
+
 }
