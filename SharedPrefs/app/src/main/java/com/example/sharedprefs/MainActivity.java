c86ab20fe4b5213bcc82139dc730dfc0c7f8b6d9
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-07-12 08:38:02 +0500

    obj created

--- SharedPrefs/app/src/main/java/com/example/sharedprefs/MainActivity.java
+++ SharedPrefs/app/src/main/java/com/example/sharedprefs/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.sharedprefs;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button saveButton;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.editTextId);
+        saveButton = (Button) findViewById(R.id.saveButtonId);
+        editText = (EditText) findViewById(R.id.editTextId);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 
+            }
+        });
+
     }
 }
