9b365f0d0879fee1f4339bf8eb4a7cf5763b113f
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-07-13 10:09:13 +0500

    table setted up

--- DBIntro/app/src/main/java/Data/DatabaseHandler.java
+++ DBIntro/app/src/main/java/Data/DatabaseHandler.java
@@ -3,22 +3,34 @@ package Data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.view.animation.AnimationUtils;
 
+import Utils.Util;
 import androidx.annotation.Nullable;
 
 class DatabaseHandler extends SQLiteOpenHelper {
 
-    public DatabaseHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHandler(@Nullable Context context) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+//        SQL - structured query language
+        String CREATE_CONTACT_TABLE = "CREATE TABLE " + Util.TABLE_NAME + "("
+                + Util.KEY_ID + " INTEGER PRIMARY KEY, " + Util.KEY_NAME + " TEXT,"
+                + Util.KEY_PHONE_NUMBER + " TEXT" + ")";
+
+        db.execSQL(CREATE_CONTACT_TABLE);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//         Dropping is deleting the table!
+        db.execSQL("DROP TABLE IF EXISTS " + Util.TABLE_NAME);
 
+//        Create table again
+        onCreate(db);
     }
 }
--- DBIntro/app/src/main/java/Utils/Util.java
+++ DBIntro/app/src/main/java/Utils/Util.java
@@ -1,16 +1,14 @@
 package Utils;
 
-class Utils {
+public class Util {
     // DataBase version
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "contactDB";
     public static final String TABLE_NAME = "contacts";
 
-//    Contacts table columns names
+    //    Contacts table columns names
     public static final String KEY_ID = "id";
-    public static final String KEY_NUMBER = "name";
+    public static final String KEY_NAME = "name";
     public static final String KEY_PHONE_NUMBER = "phone_number";
 
-
-    
 }
