43ef1674f159efb3b07b02828620cd98e34e32dc
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-07-13 09:27:31 +0500

    1

---
+++ DBIntro/app/src/main/java/Utils/Utils.java
@@ -0,0 +1,14 @@
+package Utils;
+
+class Utils {
+    // DataBase version
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "contactDB";
+    private static final String TABLE_NAME = "contacts";
+
+//    Contacts table columns names
+    private static final String KEY_ID = "id";
+    private static final String KEY_NUMBER = "name";
+    private static final String KEY_PHONE_NUMBER = "phone_number";
+
+}
--- HoneyDoList/app/src/main/java/com/example/honeydolist/MainActivity.java
+++ HoneyDoList/app/src/main/java/com/example/honeydolist/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private Button saveButton;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
