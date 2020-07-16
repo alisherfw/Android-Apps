9c4f19454eb07857c909800a64c650b47593d06c
alisherfw <57340324+alisherfw@users.noreply.github.com>
2020-07-16 11:34:56 +0500

    deleteContact() added

--- DBIntro/app/src/main/java/com/example/dbintro/MainActivity.java
+++ DBIntro/app/src/main/java/com/example/dbintro/MainActivity.java
@@ -19,27 +19,26 @@ public class MainActivity extends AppCompatActivity {
 
 //                Insert contacts
         Log.d("Insert: ", "Insertin...");
-        db.addContact(new Contact("Paul", "412424512"));
-        db.addContact(new Contact("John", "125293123"));
-        db.addContact(new Contact("Rose", "241232422"));
-        db.addContact(new Contact("Bella", "298589232"));
+//        db.addContact(new Contact("Paul", "412424512"));
+//        db.addContact(new Contact("John", "125293123"));
+//        db.addContact(new Contact("Rose", "241232422"));
+//        db.addContact(new Contact("Bella", "298589232"));
 
 //        Read them back
         Log.d("Reading",    "Reading all contacts");
         List<Contact> contactList = db.getAllContacts();
 
 //        Get 1 contact
-        Contact oneContact = db.getContact(1);
+        Contact oneContact = db.getContact(4);
         oneContact.setName("Polooooooo");
         oneContact.setPhoneNumber("00000000");
 //        Update contact
 
-        int newContact = db.updateContact(oneContact);
+//        int newContact = db.updateContact(oneContact);
+//
+//        Log.d("One Contact: ", "Update row " + String.valueOf(newContact) + " Name: " + oneContact.getName() + " Phone: " + oneContact.getPhoneNumber());
 
-        Log.d("One Contact: ", "Update row " + String.valueOf(newContact) + " Name: " + oneContact.getName() + " Phone: " + oneContact.getPhoneNumber());
-
-
-
+        db.deleteContact(oneContact);
 
         for (Contact c : contactList) {
             String log = "ID: " + c.getId()+ " , Name: " + c.getName() + ", Phone: " + c.getPhoneNumber();
